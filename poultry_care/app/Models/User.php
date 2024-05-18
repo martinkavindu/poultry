e42ac9e6215a755b8458006d448be6573b5632fd
Mutuku1998 <martinmutuku1998@gmail.com>
2024-05-18 07:00:39 +0300

    laravel spatie installation

--- poultry_care/app/Models/User.php
+++ poultry_care/app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements JWTSubject
 {
