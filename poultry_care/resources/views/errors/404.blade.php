e5639625ae92a1f05abe654578f18cf8e21bac5c
Mutuku1998 <martinmutuku1998@gmail.com>
2024-06-01 00:48:43 +0300

    mm

---
+++ poultry_care/resources/views/errors/404.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404</title>
+
+    <style>
+
+        body{
+
+            background-color:  green;
+            display: flex;
+            justify-content: center
+
+        }
+    </style>
+
+</head>
+<body>
+    
+<h2 style="align-items: center">
+   404  PAGE NOT FOUND
+</h2>
+</body>
+</html>
